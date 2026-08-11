ad3b9ea6bbefc6e91480a8370a03b390243ef539
dhav211 <dhav211@gmail.com>
2026-08-10 18:22:09 -0700

    adds more tests

--- tests/unit/test_url_db.py
+++ tests/unit/test_url_db.py
@@ -1,10 +1,32 @@
 from datetime import date
+import pytest
 
 from src.models.url import Url
 
 
-def test_create_url(db_session):
-    url = Url(full="https://docs.sqlalchemy.org/en/20/orm/quickstart.html", short="5aa32bhh", creation_date=date.today(), expiration_date=date.today())
-    db_session.add(url)
+@pytest.fixture()
+def default_urls(db_session):
+    urls = [
+        Url(full="https://docs.sqlalchemy.org/en/20/orm/quickstart.html", short="5aa32bhh", creation_date=date.today(), expiration_date=date.today()),
+        Url(full="https://github.com/dhav211/fastapi_url_shortener", short="82sg543s", creation_date=date.today(), expiration_date=date.today()),
+        Url(full="https://www.boredpanda.com/most-destructive-thing-cat-ever-done/", short="al0sf8sf", creation_date=date.today(), expiration_date=date.today())
+    ]
+    db_session.add_all(urls)
     db_session.commit()
-    assert db_session.query(Url).count() == 1
+
+
+def test_remove_url_by_short(default_urls, db_session):
+    url = db_session.query(Url).filter_by(short="82sg543s").first()
+
+    if url:
+        db_session.delete(url)
+        db_session.commit()
+
+    assert db_session.query(Url).count() == 2
+
+def test_get_all_urls(default_urls, db_session):
+    assert db_session.query(Url).count() == 3
+
+def test_get_by_short(default_urls, db_session):
+    url = db_session.query(Url).filter_by(short="82sg543s").first()
+    assert url.short == "82sg543s"
